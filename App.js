330de029158c281b0bd0eb3e708865b40ff88c51
Tsounguidev <rodriguetsoungui.2006@gmail.com>
2023-07-24 16:13:54 +0200

    creation of sign_up, sign_in and confirm_email page

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Amplify } from 'aws-amplify';
-import awsExports from './src/aws-exports';
-Amplify.configure(awsExports);
+
+
+// import { Amplify } from 'aws-amplify';
+// import awsExports from './src/aws-exports';
+// Amplify.configure(awsExports);
+
+import SignIn from './pages/singn_in';
+import SignUp from './pages/sign_up';
+import ConfirmEmailScreen from './pages/confirm_email';
+
 
 export default function App() {
+  const Stack= createNativeStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="sign_Up" component={SignUp} />
+     <Stack.Screen  name="Sign_In" component={SignIn} /> 
+     <Stack.Screen name="confirm_email" component={ConfirmEmailScreen} />
+     
+      
+    </Stack.Navigator>
+  </NavigationContainer>
+  
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
