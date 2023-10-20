ed6a2d6846e7e6285c18c46dcd9a8bcf80a0836b
Abdoulaye <njigouhrazak@iut-dhaka.edu>
2023-10-20 09:26:25 +0100

    feat: Navbar can redirect and changes car icon to localization icon

--- components/StickyFooter.js
+++ components/StickyFooter.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Feather } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 const StickyFooter = ({ title }) => {
+    const navigation = useNavigation();
+    const goBackTwoScreens = () => {
+      navigation.goBack();
+      navigation.goBack();
+    };
+
+    // Go back one screen
+    const goBackOneScreen = () => {
+      navigation.goBack(); // or navigation.pop();
+    };
+
   return (
     <View style={styles.footerContainer}>
       
+
+      <TouchableOpacity onPress={goBackTwoScreens}>
       <View style={{ alignItems: 'center' }}>
         <MaterialCommunityIcons name="account" size={25} color="black" />
         <Text style={{ marginTop: 5 }}>Profile</Text>
       </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={goBackOneScreen}>
+        <View style={{ alignItems: 'center' }}>
+          <FontAwesome5 name="child" size={25} color="black" /> 
+          <Text style={{ marginTop: 5 }}>Children</Text>
+        </View>
+      </TouchableOpacity>
       <View style={{ alignItems: 'center' }}>
-        <FontAwesome5 name="child" size={25} color="black" /> 
-        <Text style={{ marginTop: 5 }}>Children</Text>
-      </View>
-      <View style={{ alignItems: 'center' }}>
-        <Feather name="truck" size={25} color="black" />
-        <Text style={{ marginTop: 5 }}>Cars</Text>
+        <Feather name="map-pin" size={25} color="black" />
+        <Text style={{ marginTop: 5 }}>Location</Text>
       </View>
     </View>
   );
